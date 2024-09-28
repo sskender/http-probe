f0665f8d825adee2e74c45ff6176f84a8a0c36d3
Sven Skender <sskender@pm.me>
2024-09-28 15:39:04 +0200

    Improve logging

--- probe.sh
+++ probe.sh
@@ -5,15 +5,15 @@ sleep_time=10
 IFS='
 '
 
-function log_time () {
-    date +'%Y-%m-%d %H:%M:%S'
+function print_log () {
+    echo "[$(date +'%Y-%m-%d %H:%M:%S')] $1"
 }
 
 
-echo "[$(log_time)] Starting the probe process"
+print_log "Starting the probe process"
 
 if [ "$#" -ne 1 ]; then
-    echo "[$(log_time)] Invalid endpoint. Usage: $0 <http-endpoint>"
+    print_log "Invalid endpoint. Usage: $0 <http-endpoint>"
     exit 1
 fi
 
@@ -21,41 +21,41 @@ fi
 endpoint=$1
 domain=$(echo $endpoint | awk -F/ '{ print $3 }')
 
-echo "[$(log_time)] Target endpoint: $endpoint"
+print_log "Target endpoint: $endpoint"
 
 
 while [ 1 ]; do
-    echo "[$(log_time)] Sending the probe"
+    print_log "Sending the probe"
 
 
-    echo "[$(log_time)] Trying to resolve domain: $domain"
+    print_log "Trying to resolve domain: $domain"
 
     records=$(dig +short $domain)
     records_len=$(echo $records | wc -w)
 
     if [ $records_len -eq 0 ]; then
-        echo "[$(log_time)] Domain could not be resolved"
+        print_log "Domain could not be resolved"
 
-        echo "[$(log_time)] Waiting for ${sleep_time}s before sending the next probe"
+        print_log "Waiting for ${sleep_time}s before sending the next probe"
         sleep $sleep_time
 
         continue
     fi
 
     for record in $records; do
-        echo "[$(log_time)] Record found: $record"
+        print_log "Record found: $record"
     done
 
 
     response=$(curl --max-time 5 --silent --output /dev/null --write-out "%{http_code}" "$endpoint")
 
     if [ $response -eq 000 ]; then
-        echo "[$(log_time)] Service not available"
+        print_log "Service not available"
     else
-        echo "[$(log_time)] HTTP response code: $response"
+        print_log "HTTP response code: $response"
     fi
 
 
-    echo "[$(log_time)] Waiting for ${sleep_time}s before sending the next probe"
+    print_log "Waiting for ${sleep_time}s before sending the next probe"
     sleep $sleep_time
 done
